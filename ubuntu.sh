f2525485a07359e589531c3a84927a9859574c3d
HFRibeiro <ribeirh02@gmail.com>
2020-09-27 18:24:42 +0100

    add ubuntu install

---
+++ ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get -y install git make ansible xclip
+git config --global user.name "HFRibeiro"
+git config --global user.email "ribeirh02@gmail.com"
+cd ubuntu18_04 && ansible-playbook -K playbook.yml
+ssh-keygen -t rsa -b 4096 -C "ribeirh02@gmail.com"
+xclip -sel clip < ~/.ssh/id_rsa.pub
+echo "\n\n\n\nShould restart after this\n\n\n"
+echo ~/.ssh/id_rsa.pub
+chown code:code .local/share/keyrings
+chmod a+rw .local/share/keyrings
