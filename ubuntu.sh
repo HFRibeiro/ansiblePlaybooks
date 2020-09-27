3413040aabfd65955b288be28ed112b8f6d5fc11
HFRibeiro <ribeirh02@gmail.com>
2020-09-27 18:41:19 +0100

    add vm tools

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get -y install git make ansible xclip
+sudo apt-get -y install git make ansible xclip open-vm-tools-desktop
 git config --global user.name "HFRibeiro"
 git config --global user.email "ribeirh02@gmail.com"
 cd ubuntu18_04 && ansible-playbook -K playbook.yml
@@ -7,5 +7,3 @@ ssh-keygen -t rsa -b 4096 -C "ribeirh02@gmail.com"
 xclip -sel clip < ~/.ssh/id_rsa.pub
 echo "\n\n\n\nShould restart after this\n\n\n"
 echo ~/.ssh/id_rsa.pub
-chown code:code .local/share/keyrings
-chmod a+rw .local/share/keyrings
